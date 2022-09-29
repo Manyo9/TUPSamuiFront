2a0f21666dac9f257ef2cac946769479aa156696
2w1-113491-Monticoli Pablo <113491@tecnicatura.frc.utn.edu.ar>
2022-09-29 19:32:54 -0300

    add alta producto navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AltaPedidoComponent } from './pedidos/alta-pedido/alta-pedido.component';
+import { AltaProductoComponent } from './productos/alta-producto/alta-producto.component';
 
 const routes: Routes = [
   {path : 'home',component: HomeComponent},
   {path : 'pedidos/nuevo', component: AltaPedidoComponent},
-
+  {path : 'productos/nuevo',component : AltaProductoComponent}
 ];
 
 @NgModule({
