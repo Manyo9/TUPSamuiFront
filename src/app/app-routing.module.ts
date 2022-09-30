9e989e7662d17f665fc916d3b0db4b1c7f6d4c03
Manyo <agusmangini@hotmail.com>
2022-09-30 16:48:06 -0300

    Agrego rutas de login y redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 import { AltaPedidoComponent } from './pedidos/alta-pedido/alta-pedido.component';
 import { AltaProductoComponent } from './productos/alta-producto/alta-producto.component';
 
 const routes: Routes = [
-  {path : 'home',component: HomeComponent},
+  {path : 'home', component: HomeComponent},
   {path : 'pedidos/nuevo', component: AltaPedidoComponent},
-  {path : 'productos/nuevo',component : AltaProductoComponent}
+  {path : 'productos/nuevo',component : AltaProductoComponent},
+  {path : 'login', component: LoginComponent},
+  {path : '', redirectTo: 'home', pathMatch: 'full'}
 ];
 
 @NgModule({
