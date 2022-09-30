6661327338a23230b84670346a10e2a4a5ac5580
Manyo <agusmangini@hotmail.com>
2022-09-30 17:49:44 -0300

    Correciones sobre usuarios

--- src/app/models/usuario-login.ts
+++ src/app/models/usuario-login.ts
@@ -1,2 +1,4 @@
 export class UsuarioLogin {
+    nombre: string;
+    contrasenia: string;
 }
--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -8,6 +8,8 @@ import { UsuarioLogin } from '../models/usuario-login';
 export class UsuarioService {
   private recurso: string = 'usuarios'
   private apiUrl: string = `https://633617a38aa85b7c5d282607.mockapi.io/api/${this.recurso}`;
+
+
   constructor(private http: HttpClient) { }
 
   obtenerUsuarios(): Observable<Usuario[]> {
@@ -16,11 +18,12 @@ export class UsuarioService {
 
   // horrible feo feísimo
   // implementar con POST cuando tengamos la API con springboot
+  // solo matchea con el nombre
   login(usuario: UsuarioLogin): Observable<Usuario> {
     return this.http.get<Usuario>(`${this.apiUrl}?nombre=${usuario.nombre}&contrasenia=${usuario.contrasenia}`);
   }
 
   obtenerUnUsuario(id: number): Observable<Usuario>{
     return this.http.get<Usuario>(`${this.apiUrl}/${id}`)
-  } 
+  }
 }
