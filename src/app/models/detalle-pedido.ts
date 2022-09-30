0a3f4d667b7b10bf4a783bfc3f4420b7c13e9429
Abraguas <fmoises2002@gmail.com>
2022-09-30 20:29:18 -0300

    Creo clases Pedido y DetallePedido

---
+++ src/app/models/detalle-pedido.ts
@@ -0,0 +1,12 @@
+import { Producto } from "./producto";
+
+export class DetallePedido {
+    idDetalle?: number;
+    idPedido?: number;
+    producto: Producto;
+    idPromocion?: number;
+    cantidad: number;
+    precioUnitario: number;
+    porcentajeDescuento?: number;
+    puntosGanados?: number;
+}
