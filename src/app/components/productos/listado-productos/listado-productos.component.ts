7a5e158bf389a6c2a12abb95d26c6ca5d840c616
PMonticoli <113491@tecnicatura.frc.utn.edu.ar>
2022-10-28 18:49:59 -0300

    Implemento listado productos

--- src/app/components/productos/listado-productos/listado-productos.component.ts
+++ src/app/components/productos/listado-productos/listado-productos.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Producto } from 'src/app/models/producto';
+import { ResultadoGenerico } from 'src/app/models/resultado-generico';
+import { ProductoService } from 'src/app/services/producto.service';
 
 @Component({
   selector: 'app-listado-productos',
   templateUrl: './listado-productos.component.html',
   styleUrls: ['./listado-productos.component.css']
 })
-export class ListadoProductosComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class ListadoProductosComponent implements OnDestroy,OnInit {
+  @Input() listado : Producto[]=[];
+  constructor(private servicioProducto : ProductoService) { }
+  private subscription = new Subscription();
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+  ngOnInit() : void{
+    this.actualizarListado();
   }
 
+  actualizarListado(){
+    this.subscription.add(
+      this.servicioProducto.obtenerTodos().subscribe({
+        next : (listado: ResultadoGenerico) =>{    
+          if(listado.resultado && listado.resultado.length>0){
+            this.listado=listado.resultado;
+          }
+        },
+        error :() =>{
+          alert('Error al actualizar listado productos')
+        }
+      })
+    )
+  }
 }
