bb9c4f278205413f87aa83b67aae699faf4c1721
Abraguas <fmoises2002@gmail.com>
2022-10-29 02:27:01 -0300

    Implemento API en agregar pedido

--- src/app/services/pedido.service.ts
+++ src/app/services/pedido.service.ts
@@ -1,17 +1,27 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Pedido } from '../models/pedido';
+import { ResultadoGenerico } from '../models/resultado-generico';
 
 @Injectable()
 export class PedidoService {
 
-  private API_URL : string = 'https://632b1463713d41bc8e7fdd8b.mockapi.io/wawa/pedidos';
+  private API_URL : string = 'http://localhost:3000/pedidos/';
   constructor(private http : HttpClient) { }
 
 
-  agregar(pedido : Pedido) : Observable<Pedido>{
-    return this.http.post<Pedido>(this.API_URL,pedido);
+  agregar(pedido : Pedido) : Observable<ResultadoGenerico>{
+    let auth_token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+
+        'Content-Type': 'application/json',
+
+        'Authorization': `Bearer ${auth_token}`
+
+      });
+    const requestOptions = { headers: headers };
+    return this.http.post<ResultadoGenerico>(this.API_URL,pedido,requestOptions);
   }
   obtenerPendientes() : Observable<Pedido[]>{
     return this.http.get<Pedido[]>(this.API_URL + "?idEstado=1");
