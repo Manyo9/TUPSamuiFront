d43984caed1bf5653cc1788bc4bd35f1ee400742
2w1-113491-Monticoli Pablo <113491@tecnicatura.frc.utn.edu.ar>
2022-11-01 14:41:06 -0300

    Implemento listado gustos

--- src/app/components/gustos/listado-gustos/listado-gustos.component.ts
+++ src/app/components/gustos/listado-gustos/listado-gustos.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,Input,OnDestroy } from '@angular/core';
+import { Gusto } from 'src/app/models/gusto';
+import { GustoService } from 'src/app/services/gusto.service';
+import { Subscription } from 'rxjs';
+import { ResultadoGenerico } from 'src/app/models/resultado-generico';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-listado-gustos',
   templateUrl: './listado-gustos.component.html',
   styleUrls: ['./listado-gustos.component.css']
 })
-export class ListadoGustosComponent implements OnInit {
+export class ListadoGustosComponent implements OnInit,OnDestroy {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  @Input() listado : Gusto[]=[];
+  
+  constructor(private servicioGusto : GustoService) { }
+  private subscription = new Subscription();
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+  ngOnInit() : void{
+    this.actualizarListado();
   }
 
+  actualizarListado(){
+    this.subscription.add(
+      this.servicioGusto.obtenerTodos().subscribe({
+        next : (listado: ResultadoGenerico) =>{    
+          if(listado.resultado && listado.resultado.length>=0){ 
+            this.listado=listado.resultado;
+          }
+        },
+        error :() =>{
+          alert('Error al actualizar listado gustos')
+        }
+      })
+    )
+  }
+
+  nuevoGusto(){
+    
+  }
 }
