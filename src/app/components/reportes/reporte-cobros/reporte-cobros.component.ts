1e02a3178f8a4de1862b202ca13d774dc1a7d84f
2w1-113491-Monticoli Pablo <113491@tecnicatura.frc.utn.edu.ar>
2022-11-05 09:27:13 -0300

    Creo componente reporte cobros

---
+++ src/app/components/reportes/reporte-cobros/reporte-cobros.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reporte-cobros',
+  templateUrl: './reporte-cobros.component.html',
+  styleUrls: ['./reporte-cobros.component.css']
+})
+export class ReporteCobrosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
