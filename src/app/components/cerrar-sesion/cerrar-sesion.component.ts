ef18ab83f752b3a75c502512a6ebb1a6b4a694ff
Manyo <agusmangini@hotmail.com>
2022-11-12 17:00:45 -0300

    implemento sweet alert en func confirm

--- src/app/components/cerrar-sesion/cerrar-sesion.component.ts
+++ src/app/components/cerrar-sesion/cerrar-sesion.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SesionIniciadaService } from '../../services/sesion-iniciada.service';
+import { SweetAlert } from 'sweetalert/typings/core';
+const swal: SweetAlert = require('sweetalert');
 
 @Component({
   selector: 'app-cerrar-sesion',
@@ -14,11 +16,25 @@ export class CerrarSesionComponent implements OnInit {
   ngOnInit(): void {
   }
   cerrarSesion(): void {
-    if (confirm("¿Seguro desea cerrar la sesión?")){
-      localStorage.removeItem('token');
-      this.sesionService.cambiarEstado(false);
-      this.router.navigate(['home']);
-    }
-
+    swal({
+      title: "Cerrar sesión",
+      text: "¿Seguro que quiere cerrar la sesión?",
+      icon: "warning",
+      dangerMode: true,
+      buttons: {
+        cancel: true,
+        confirm: true,
+      }
+    })
+    .then((cerrarSesion) => {
+      if (cerrarSesion) {
+        localStorage.removeItem('token');
+        this.sesionService.cambiarEstado(false);
+        this.router.navigate(['home']);
+        swal("Hasta pronto!", {
+          icon: "success",
+        });
+      }
+    });
   }
 }
